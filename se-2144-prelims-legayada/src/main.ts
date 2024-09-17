45a5e6cef0fe597d26f22c6004d83d8d3c2189f6
A. Legayada <tonilegayada@gmail.com>
2024-09-17 18:10:52 +0800

    typescript notes
    notes for the functions and requirements

--- se-2144-prelims-legayada/src/main.ts
+++ se-2144-prelims-legayada/src/main.ts
@@ -1,1 +1,12 @@
 //write main functionality here
+
+/**
+ * Input will not be invokign the  active
+ * if active then change teh display
+ * get teh value of teh display and run an eval when clicking the equal button
+ * the hello wll display from an array using choice
+ * the goodbye will turn the input background to black and will run a settimeout function that
+ * replacaes the value of it to 'goodbye'
+ * if you click on the ac and it's active it will clear the display
+ * if it's inactive will turn the calculator on
+ */
